fefc72c2f84b7f2134a2f26f5844c315959f3d7f
Pol Pereta <cartme.backend@gmail.com>
2018-12-21 16:40:09 +0100

    Domain exceptions added.

--- src/Domain/VerificationVerifiedAlready.php
+++ src/Domain/VerificationVerifiedAlready.php
@@ -2,6 +2,12 @@
 
 namespace App\Domain;
 
+/**
+ * Verification verified exception.
+ *
+ * @author: Pol Pereta Quintana <polpereta@gmail.com>
+ * @since: 21/12/18
+ */
 class VerificationVerifiedAlready extends DomainException
 {
 
